f40fda684c3678179c75faf0687fdf2afb9d6c86
mxc-maggiechen <mxc.maggiechen@gmail.com>
2024-06-12 17:20:02 -0400

    added sample data in prisma

--- frontend/src/app/api/sample/route.ts
+++ frontend/src/app/api/sample/route.ts
@@ -1,9 +1,22 @@
-import {NextResponse} from 'next/server'
-import characters from '@/data/characters.json';
+import { NextResponse } from 'next/server'
+import db from '../../../../../backend/lib/prisma'
 
-export async function GET() {
-        return NextResponse.json({
-            characters: characters.data
-        }
-    )
+export async function GET(req: Request) {
+  return NextResponse.json({
+   hello: "world"
+  })
+}
+
+export async function POST(req: Request) {
+  const body = req.body
+  console.log("received")
+  const user = await db.user.findUnique({
+    where:{
+        id: 1
+    }
+  })
+
+  return NextResponse.json({
+    message: user.email,
+  })
 }
