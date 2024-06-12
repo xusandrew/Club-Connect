93615b006a50e185aba4a15ad37a4d4d2667d1f0
mxc-maggiechen <mxc.maggiechen@gmail.com>
2024-06-12 17:20:30 -0400

    added sample data in prisma

---
+++ backend/lib/prisma.ts
@@ -0,0 +1,16 @@
+//global prisma client
+
+import { PrismaClient } from '@prisma/client';
+
+let db: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  db = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  db = global.prisma;
+}
+
+export default db;
