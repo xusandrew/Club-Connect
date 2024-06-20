a3eb6c6938e53482ff713c268e0070298811d0c7
Andrew Xu <xusandrew@gmail.com>
2024-06-20 12:15:09 -0400

    Generate output for sample queries

--- sample-queries/createPost.sql
+++ sample-queries/createPost.sql
@@ -1,7 +1,35 @@
-INSERT INTO Event
---Primary Key is auto generate I believe--
-VALUES (222, "Bingo", "Come and play Bingo with us!", "SLC", "2024-06-30 18:00:00", "2024-06-30 20:00:00", "2024-06-20 18:00:00");
+INSERT INTO
+    "Event" (
+        cid,
+        title,
+        description,
+        location,
+        start_time,
+        end_time,
+        posted_time
+    )
+VALUES
+    (
+        1,
+        'Bingo',
+        'Come and play Bingo with us!',
+        'SLC',
+        '2024-06-30 18:00:00',
+        '2024-06-30 20:00:00',
+        '2024-06-20 18:00:00'
+    );
 
---Just for Output purposes--
-SELECT * FROM Event;
+SELECT
+    *
+FROM
+    "Event";
 
+-- Output
+--      title      |               description               |   location   |     start_time      |      end_time       |       posted_time       | eid | cid 
+-- ----------------+-----------------------------------------+--------------+---------------------+---------------------+-------------------------+-----+-----
+--  Tech Talk      | An event to discuss the latest in tech. | Room 101     | 2024-07-01 10:00:00 | 2024-07-01 12:00:00 | 2024-06-20 15:29:33.368 |   1 |   1
+--  Art Exhibition | An exhibition of student artwork.       | Art Gallery  | 2024-07-02 10:00:00 | 2024-07-02 12:00:00 | 2024-06-20 15:29:33.368 |   2 |   2
+--  Football Match | A friendly football match.              | Sports Field | 2024-07-03 10:00:00 | 2024-07-03 12:00:00 | 2024-06-20 15:29:33.368 |   3 |   3
+--  Music Concert  | A concert featuring local bands.        | Auditorium   | 2024-07-04 10:00:00 | 2024-07-04 12:00:00 | 2024-06-20 15:29:33.368 |   4 |   4
+--  Bingo          | Come and play Bingo with us!            | SLC          | 2024-06-30 18:00:00 | 2024-06-30 20:00:00 | 2024-06-20 18:00:00     |   6 |   1
+-- (5 rows)
--- sample-queries/editPost.sql
+++ sample-queries/editPost.sql
@@ -1,10 +1,35 @@
-UPDATE Event
-SET description = "BINGO CANCELLED",
-    location = "SLC",
-    start-time = "2024-06-30 18:00:00",
-    end-time = "2024-06-30 20:00:00",
-    posted-time = "2024-06-20 18:00:00"
-WHERE eID = 222; 
+SELECT
+    *
+FROM
+    "Event";
 
---Just for Output purposes--
-SELECT * FROM Event;
+UPDATE "Event"
+SET
+    description = 'BINGO CANCELLED',
+    location = 'SLC',
+    start_time = '2024-06-30 18:00:00',
+    end_time = '2024-06-30 20:00:00',
+    posted_time = '2024-06-20 18:00:00'
+WHERE
+    eid = 1;
+
+SELECT
+    *
+FROM
+    "Event";
+
+-- Output
+--      title      |               description               |   location   |     start_time      |      end_time       |       posted_time       | eid | cid 
+-- ----------------+-----------------------------------------+--------------+---------------------+---------------------+-------------------------+-----+-----
+--  Tech Talk      | An event to discuss the latest in tech. | Room 101     | 2024-07-01 10:00:00 | 2024-07-01 12:00:00 | 2024-06-20 16:12:00.022 |   1 |   1
+--  Art Exhibition | An exhibition of student artwork.       | Art Gallery  | 2024-07-02 10:00:00 | 2024-07-02 12:00:00 | 2024-06-20 16:12:00.022 |   2 |   2
+--  Football Match | A friendly football match.              | Sports Field | 2024-07-03 10:00:00 | 2024-07-03 12:00:00 | 2024-06-20 16:12:00.022 |   3 |   3
+--  Music Concert  | A concert featuring local bands.        | Auditorium   | 2024-07-04 10:00:00 | 2024-07-04 12:00:00 | 2024-06-20 16:12:00.022 |   4 |   4
+-- (4 rows)
+-- UPDATE 1
+--      title      |            description            |   location   |     start_time      |      end_time       |       posted_time       | eid | cid 
+-- ----------------+-----------------------------------+--------------+---------------------+---------------------+-------------------------+-----+-----
+--  Art Exhibition | An exhibition of student artwork. | Art Gallery  | 2024-07-02 10:00:00 | 2024-07-02 12:00:00 | 2024-06-20 16:12:00.022 |   2 |   2
+--  Football Match | A friendly football match.        | Sports Field | 2024-07-03 10:00:00 | 2024-07-03 12:00:00 | 2024-06-20 16:12:00.022 |   3 |   3
+--  Music Concert  | A concert featuring local bands.  | Auditorium   | 2024-07-04 10:00:00 | 2024-07-04 12:00:00 | 2024-06-20 16:12:00.022 |   4 |   4
+--  Tech Talk      | BINGO CANCELLED                   | SLC          | 2024-06-30 18:00:00 | 2024-06-30 20:00:00 | 2024-06-20 18:00:00     |   1 |   1
