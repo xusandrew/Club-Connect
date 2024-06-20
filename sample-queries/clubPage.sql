d930ff7149080c3b8a97b151a44b64da41214a9b
Andrew Xu <xusandrew@gmail.com>
2024-06-20 12:15:09 -0400

    Generate output for sample queries

--- sample-queries/clubPage.sql
+++ sample-queries/clubPage.sql
@@ -1,4 +1,21 @@
-SELECT Club.description, Club.instagra, Club.discord, Event.title, Event.description, Event.location, Event.start-time, Event.end-time, Event.posted-time
-FROM Club JOIN Event ON Club.cID = Event.cID
-WHERE Club.cID = 111;
+SELECT
+    "Club".description,
+    "Club".instagram,
+    "Club".discord,
+    "Event".title,
+    "Event".description,
+    "Event".location,
+    "Event".start_time,
+    "Event".end_time,
+    "Event".posted_time
+FROM
+    "Club"
+    JOIN "Event" ON "Club".cid = "Event".cid
+WHERE
+    "Club".cid = 2;
 
+-- Output
+--       description       | instagram | discord |     title      |            description            |  location   |     start_time      |      end_time       |       posted_time       
+-- ------------------------+-----------+---------+----------------+-----------------------------------+-------------+---------------------+---------------------+-------------------------
+--  A club for art lovers. |           |         | Art Exhibition | An exhibition of student artwork. | Art Gallery | 2024-07-02 10:00:00 | 2024-07-02 12:00:00 | 2024-06-20 15:29:33.368
+-- (1 row)
