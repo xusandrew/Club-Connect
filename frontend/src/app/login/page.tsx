828ad61566305e6733ab81c28c5e2bc03abd4236
Andrew Xu <xusandrew@gmail.com>
2024-07-07 15:47:21 -0400

    Style login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,23 +1,11 @@
 import { login } from '@/auth'
-import Link from 'next/link'
+import { Login } from 'data/components/login'
 
 const LoginPage: React.FC = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <form action={login}>
-        <div>
-          <label htmlFor='email'>Email:</label>
-          <input className='text-black' type='text' id='email' name='email' />
-        </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input className='text-black' type='password' id='password' name='password' />
-        </div>
-        <button type='submit'>Login</button>
-      </form>
-      <Link href='/register'>Click here to register your club</Link>
-    </div>
+    <form action={login}>
+      <Login />
+    </form>
   )
 }
 
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,8 +3,9 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './data/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -12,66 +13,45 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-    },
       colors: {
-            "border": "hsl(var(--border))",
-            "input": "hsl(var(--input))",
-            "ring": "hsl(var(--ring))",
-            "background": "hsl(var(--background))",
-            "foreground": "hsl(var(--foreground))",
-            "primary": {
-              "DEFAULT": "hsl(var(--primary))",
-              "foreground": "hsl(var(--primary-foreground))"
-            },
-            "secondary": {
-              "DEFAULT": "hsl(var(--secondary))",
-              "foreground": "hsl(var(--secondary-foreground))"
-            },
-            "destructive": {
-              "DEFAULT": "hsl(var(--destructive))",
-              "foreground": "hsl(var(--destructive-foreground))"
-            },
-            "muted": {
-              "DEFAULT": "hsl(var(--muted))",
-              "foreground": "hsl(var(--muted-foreground))"
-            },
-            "accent": {
-              "DEFAULT": "hsl(var(--accent))",
-              "foreground": "hsl(var(--accent-foreground))"
-            },
-            "popover": {
-              "DEFAULT": "hsl(var(--popover))",
-              "foreground": "hsl(var(--popover-foreground))"
-            },
-            "card": {
-              "DEFAULT": "hsl(var(--card))",
-              "foreground": "hsl(var(--card-foreground))"
-            }
-          },
-      borderRadius: {
-            "lg": "var(--radius)",
-            "md": "calc(var(--radius) - 2px)",
-            "sm": "calc(var(--radius) - 4px)"
-          },
-      keyframes: {
-            "accordion-down": {
-              "from": {
-                "height": "0"
-              },
-              "to": {
-                "height": "var(--radix-accordion-content-height)"
-              }
-            },
-            "accordion-up": {
-              "from": {
-                "height": "var(--radix-accordion-content-height)"
-              },
-              "to": {
-                "height": "0"
-              }
-            }
-          }
-},
-  plugins: [require("tailwindcss-animate")],
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        destructive: 'var(--destructive)',
+        accent: 'var(--accent)',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        input: 'var(--input)',
+        border: 'var(--border)',
+        ring: 'var(--ring)',
+        muted: 'var(--muted)',
+        popover: 'var(--popover)',
+        card: 'var(--card)',
+      },
+    },
+    borderRadius: {
+      lg: 'var(--radius)',
+      md: 'calc(var(--radius) - 2px)',
+      sm: 'calc(var(--radius) - 4px)',
+    },
+    keyframes: {
+      'accordion-down': {
+        from: {
+          height: '0',
+        },
+        to: {
+          height: 'var(--radix-accordion-content-height)',
+        },
+      },
+      'accordion-up': {
+        from: {
+          height: 'var(--radix-accordion-content-height)',
+        },
+        to: {
+          height: '0',
+        },
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
 }
 export default config
