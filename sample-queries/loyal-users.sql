cbfbe050f796d850defc973657eaa0f03c9af442
Andrew Xu <xusandrew@gmail.com>
2024-07-08 06:39:36 -0400

    update seed and outputs for queries

--- sample-queries/loyal-users.sql
+++ sample-queries/loyal-users.sql
@@ -1,14 +1,23 @@
-SELECT r.email
-FROM RSVP r
-WHERE r.email IN (
-    SELECT r2.email
-    FROM RSVP r2
-    JOIN Event e ON r2.eID = e.eID
-    WHERE e.cID = [specific_club_id]
-    GROUP BY r2.email
-    HAVING COUNT(DISTINCT e.eID) = (
-        SELECT COUNT(*)
-        FROM Event
-        WHERE cID = [specific_club_id]
-    )
-);
+SELECT DISTINCT
+    r.email
+FROM
+    "RSVP" r
+WHERE
+    r.email IN (
+        SELECT
+            r2.email
+        FROM
+            "RSVP" r2
+            JOIN "Event" e ON r2.eID = e.eID
+        GROUP BY
+            r2.email
+        HAVING
+            COUNT(*) = (
+                SELECT
+                    COUNT(*)
+                FROM
+                    "Event"
+                WHERE
+                    cID = 1
+            )
+    );
--- sample-queries/popularity-sort.sql
+++ sample-queries/popularity-sort.sql
@@ -1,6 +1,14 @@
-SELECT e.eID, e.title, e.description, e.start_time, e.end_time, COUNT(r.email) AS rsvp_count 
-FROM Event e LEFT JOIN RSVP r ON e.eID = r.eID 
-GROUP BY e.eID --does it work without this line?????????????
-ORDER BY rsvp_count DESC;
-
-
+SELECT
+    e.eID,
+    e.title,
+    e.description,
+    e.start_time,
+    e.end_time,
+    COUNT(r.email) AS rsvp_count
+FROM
+    "Event" e
+    LEFT JOIN "RSVP" r ON e.eID = r.eID
+GROUP BY
+    e.eID
+ORDER BY
+    rsvp_count DESC;
