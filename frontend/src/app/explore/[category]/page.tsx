e5ffe9c86b4b935a85ebcf7809b8206c212c75e0
Simon Zhang <77755696+SimonZhang04@users.noreply.github.com>
2024-07-13 14:04:13 -0400

    Added search function to explore page

--- frontend/src/app/explore/[category]/page.tsx
+++ frontend/src/app/explore/[category]/page.tsx
@@ -1,21 +1,24 @@
 import Filter from '@/components/ui/FilterBar'
 import ClubCard from '@/components/explore/ClubCard'
 import { fetchCategories, fetchClubs } from '@/lib/data'
+import Search from '@/components/explore/Search'
 
 export default async function FilteredPage({
   params,
 }: {
-  params: { category?: string | undefined }
+  params: { category?: string | undefined; query?: string | undefined }
 }) {
   const category = params.category || ''
+  const query = params.query || ''
   const categories = await fetchCategories()
-  const clubs = await fetchClubs(category)
+  const clubs = await fetchClubs(category, query)
 
   return (
     <>
       <section className='w-full py-4 md:py-10 lg:py-20'>
         <div className='container px-4 md:px-6 text-center space-y-4'>
           <h1 className='text-2xl font-bold tracking-tighter sm:text-5xl'>Clubs Directory</h1>
+          <Search placeholder='test' />
         </div>
       </section>
       <div className='flex h-full flex-col md:flex-row'>
--- frontend/src/components/explore/Search.tsx
+++ frontend/src/components/explore/Search.tsx
@@ -26,7 +26,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         Search
       </label>
       <input
-        className='peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
+        className='peer block w-full rounded-md border text-black border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value)
